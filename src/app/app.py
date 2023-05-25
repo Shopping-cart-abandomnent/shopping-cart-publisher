9c5662af7d3abec05a071da5bedf852887cd0def
Pierre-Ange Billa <ftnpiapia@gmail.com>
2023-05-25 17:06:31 +0200

    init Flask app

---
+++ src/app/app.py
@@ -0,0 +1,16 @@
+from random_transaction import send_random_abandonned_cart
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__,  template_folder='templates')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/abandoned-cart')
+def abandoned_cart():
+    cart_items = send_random_abandonned_cart()
+    return render_template('abandoned_cart.html', cart_items=cart_items)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
--- src/random_transaction.py
+++ src/random_transaction.py
@@ -1,9 +1,12 @@
 import json
 from typing import List
 from google.cloud import bigquery
-from src.pub_sub import send_to_pubsub
+import sys
 
+sys.path.append('C:/Users/mgbt9/Desktop/ESME/4eANNEE/PROJET/projet/shopping-cart-publisher-main/src')
+from pub_sub import send_to_pubsub
 import os
+
 os.environ["GCLOUD_PROJECT"] = "valued-decker-380221"
 
 
@@ -15,9 +18,9 @@ def get_articles_data():
     all_articles = client.query(query).to_dataframe()
     return all_articles
 
+
 # Initialize BigQuery client
 client = bigquery.Client()
-
 
 
 def get_random_user_id():
@@ -35,15 +38,16 @@ def get_random_user_id():
 
 
 def create_message(user_id: str, article_id: List[int]) -> str:
-
     message = {
         "user_id": user_id,
         "articles_id": [int(article) for article in article_id]
     }
     return json.dumps(message)
 
+
 # Load articles from CSV into a DataFrame
 articles_df = get_articles_data()
+
 
 def send_random_abandonned_cart():
     # Randomly assign 3 articles to the user
@@ -54,7 +58,7 @@ def send_random_abandonned_cart():
     user_id = get_random_user_id()
     print("User ID:", user_id)
 
-    article_ids=list(random_articles['article_id'].values)
+    article_ids = list(random_articles['article_id'].values)
     message = create_message(user_id, article_ids)
     send_to_pubsub(message)
 
